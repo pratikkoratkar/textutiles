fd1f5d021282cfe18ccff3a7118f1c19ee86c9ba
Pratik Koratkar <86914769+pratikkoratkar@users.noreply.github.com>
2021-10-18 10:58:15 +0530

    first commit of react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import About from './components/About';
+import React,{ useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import About from './components/About';
 
 function App() {
+  const [Mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showalert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode =()=>{
+    if (Mode==='light') {
+      setMode('dark')
+      document.body.style.backgroundColor='#2c3137'
+      showalert("  Dark mode has been enabled","  Success");
+      document.title="Textutiles - Dark Mode"
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showalert("  Light mode has been enabled","  Success");
+      document.title="Textutiles - light Mode"
+
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <>
+    <Router>
+    <Navbar title="Textutiles App" mode={Mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+
+          <Route path="/">
+          <TextForm showAlert={showalert} heading="Enter The Text To Analyze" mode={Mode}/>
+          </Route>
+
+    </Switch>
+    </Router>
+    
+    </>
+
+
   );
 }
 
